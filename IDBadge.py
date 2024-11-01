fa63eca0afc173cf5ca68ea117337473e5c130b7
Enrique Rodríguez <70597078+kirodriguezke@users.noreply.github.com>
2024-11-01 14:16:52 +0100

    Add files via upload

---
+++ IDBadge.py
@@ -0,0 +1,28 @@
+print("Please enter the following information")
+print()
+
+first_name = input("First name: ")
+last_name = input("Last name: ")
+email = input("Email address: ")
+phone = input ("Phone number: ")
+job_title = input("Job title: ")
+id_number = input("ID number: ")
+
+#Additional information
+hair_color = input("Hair color: ")
+eyes_color = input("Eye color: ")
+month = input("Starting month: ")
+training = input("Completed additional training? ")
+
+print("\nThe ID Card is:")
+print("-----------------------------")
+print(f"{last_name.upper()}, {first_name.title()}")
+print(job_title)
+print(f"ID: {id_number}")
+print()
+print(email.lower())
+print(phone)
+print()
+print(f"Hair: {hair_color} Eyes: {eyes_color}")
+print(f"Month: {month} Training: {training}")
+print("-----------------------------")
